58f16b5b9a12acbce9b58385a1cfd1c70adb7bc0
rater <rater193@gmail.com>
2022-05-28 18:13:15 +0200

    Initial Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+let cfg = {
+    token: {
+        bot: "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        rest: "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    },
+    ids: {
+        bot: "976649038092980244",
+        TheOreStoreSB: "974280624594649178"
+    }
+}
+
+module.exports = cfg;
